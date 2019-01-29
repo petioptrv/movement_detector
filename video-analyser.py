ee2622ae06f40f4fa192673154d7de943aa86683
Bruno Robert <bruno.jeanluke@gmail.com>
2019-01-28 19:08:32 -0500

    Added motion detection

--- video-analyser.py
+++ video-analyser.py
@@ -1,6 +1,7 @@
 import cv2
 import logging
 import numpy as np
+import imutils
 
 # create the logger object
 logger = logging.getLogger('main')
@@ -48,53 +49,112 @@ def frame_diff_2(img_old: np.ndarray, img_new: np.ndarray) -> np.ndarray:
 
 
 def skip_frames(x: int) -> None:
+    """
+    Skips X frames
+    :param x: number of frames to skip
+    """
     for i in range(0, x):
         cam.read()
 
 
-video_path = 'videos/Rat Flashing light.wmv'
+def frame_preprocessing(frame: np.ndarray) -> np.ndarray:
+    """
+    Applies preprocessing to a frame. Converts to greyscale and applies a blur.
+    :param frame: frame to process
+    :return: the processed frame
+    """
+
+    # TODO: Add flicker reduction (see comments below)
+    # find the location of the brightest area
+    # high_loc = base_frame.argmax()
+    # low_loc = base_frame.argmin()
+    # cv2.BackgroundSubtractorMOG2()
+    # find the location of the darkest area
+    # check over multiple frames to make sure the rat isn't in the target areas
+    #
+    # for each following frame, make sure the colour intensities in the target areas
+    # match the target areas in the base frames
+    #
+    # If the intensities don't match, compute the appropriate logarithmic scale to
+    # remap the image's colour intensity.
+
+    output = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to GreyScale
+    output = cv2.GaussianBlur(output, (21, 21), 0)  # blur the image to remove high freq noise
+    return output
+
+
+def frame_postprocessing(frame: np.ndarray) -> np.ndarray:
+    """
+    Applies postprocessing to frame. Applies a threshold and then dilates the result.
+    :param frame: the to process
+    :return: the processed frame
+    """
+    output = cv2.threshold(frame, 15, 255, cv2.THRESH_BINARY)[1]
+    output = cv2.dilate(output, None, iterations=2)
+    return output
+
+
+def find_contours(frame: np.ndarray):
+    """
+    Finds contours in a frame
+    :param frame: frame to search for contours
+    :rtype: object
+    """
+    output = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL,
+                              cv2.CHAIN_APPROX_SIMPLE)
+    output = imutils.grab_contours(output)
+    return output
+
+
+video_path = 'videos/Rat Sound.wmv'
 cam = cv2.VideoCapture(video_path)
 frames = []
 frame_id = -1
 
 # skip to the part of the video with flickering
-skip_frames(4005)
+skip_frames(1000)  # 4005 for the light flickering video
 
 # initialise the array (we will be reading at least 4 frames at a time so they need to be in the array)
 frames_to_analyse = 4
 for i in range(0, frames_to_analyse):
     frame_id += 1
     ret, frame = cam.read()
+    frame = frame_preprocessing(frame)
     frames += [frame]
 
-base_frame = frames[0]  # This frame serves as a reference for the overall lighting
-
-# TODO: Add flicker reduction (see comments below)
-# find the location of the brightest area
-# find the location of the darkest area
-# check over multiple frames to make sure the rat isn't in the target areas
-#
-# for each following frame, make sure the colour intensities in the target areas
-# match the target areas in the base frames
-#
-# If the intensities don't match, compute the appropriate logarithmic scale to
-# remap the image's colour intensity.
+# TODO: allow user to select the base frame
+base_frame = frames[0]  # This frame as a reference frame, it should contain the static background
 
 while True:  # while there are frames
+    text = "Still"
     frame_id += 1
     ret, frame = cam.read()  # read the current frame
+    display_frame = frame
+    frame = frame_preprocessing(frame)
     frames += [frame]
     if not ret:  # break if there is no frame left
         break
 
     # frame diff results
-    diff4 = frame_diff_4(frames[frame_id-3], frames[frame_id-2], frames[frame_id-1], frame)
-    diff3 = frame_diff_3(frames[frame_id-2], frames[frame_id-1], frame)
-    diff2 = frame_diff_2(frames[frame_id-1], frame)
+    diff = frame_diff_2(frames[frame_id - 1], frame)
+    diff = frame_postprocessing(diff)
 
+    cnts = find_contours(diff)
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < 250:  # 250 is the min area
+            continue
+
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(display_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Moving"
+
+    cv2.putText(display_frame, "Rat Status: {}".format(text), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     # cv2.imshow displays the images
-    cv2.imshow('input frame', frame)
-    cv2.imshow('frame_diff_2', diff2)
-    cv2.imshow('frame_diff_3', diff3)
-    cv2.imshow('frame_diff_4', diff4)
+    cv2.imshow('input frame', display_frame)
+    cv2.imshow('frame_diff_3', diff)
     cv2.waitKey()  # waits until the user presses a key

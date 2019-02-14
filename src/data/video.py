75ea51f05ce2324a5e569d568a594cd1abd1c380
petioptrv <petioptrv@icloud.com>
2019-02-14 11:01:04 -0500

    Some changes.

--- src/data/video.py
+++ src/data/video.py
@@ -8,63 +8,82 @@ class AbstractVideo(ABC):
     def __init__(self, file_path: str = None):
         super().__init__()
         self.file_path = file_path
+        self.frame_count: int = None
         pass
 
     @abstractmethod
-    def next_frame(self) -> np.ndarray:
+    def current_frame(self):
+        pass
+
+    @abstractmethod
+    def next_frame(self):
         pass
 
     @abstractmethod
     def reset_frames(self):
+        pass
+
+
+class AbstractVideoPlayer(ABC):
+    def __init__(self):
+        super().__init__()
+        self.playing = False
+
+    @abstractmethod
+    def load_video(self, video: AbstractVideo):
+        pass
+
+    @abstractmethod
+    def play_video(self):
+        pass
+
+    @abstractmethod
+    def stop_video(self):
+        pass
+
+    def trigger_video(self):
+        if self.playing:
+            self.stop_video()
+        else:
+            self.play_video()
+
+    @abstractmethod
+    def reset_video(self):
         pass
 
 
 class OpenCvVideo(AbstractVideo):
-    def __init__(self, file_path: str, buffer_at_load: bool = True):
+    def __init__(self, file_path: str):
         super().__init__(file_path)
         self.video = cv2.VideoCapture(self.file_path)
-        self.frames_bgr = None
-        self.frames_grey = None
-        self.buffered = False
-        self.buff_idx = 0
+        self.frame_count = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frames_bgr: np.ndarray = None
+        self.frames_grey: np.ndarray = None
+        self._frame_idx = 0
 
-        if buffer_at_load:
-            self._buffer_video()
+        self._buffer_video()
 
     def _buffer_video(self):
-        frame_count = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        first_frame_bgr, first_frame_grey = self.next_frame()
-
-        # initialize the BGR frames array
-        bgr_array_shape = [frame_count] + list(first_frame_bgr.shape)
+        ret, frame_bgr = self.video.read()
+        bgr_array_shape = [self.frame_count] + list(frame_bgr.shape)
+        grey_array_shape = bgr_array_shape[:-1]  # remove last dimension for BGR elements
         self.frames_bgr = np.ndarray(bgr_array_shape)
-
-        # initialize the Grey-scale array
-        grey_array_shape = [frame_count] + list(first_frame_grey.shape)
         self.frames_grey = np.ndarray(grey_array_shape)
 
-        next_frame_bgr, next_frame_grey = self.next_frame()
-
-        while next_frame_bgr:
-            self.frames_bgr[len(self.frames_bgr)] = next_frame_bgr
-            self.frames_grey[len(self.frames_grey)] = next_frame_grey
-            next_frame_bgr, next_frame_grey = self.next_frame()
-
-        self.buffered = True
-
-    def next_frame(self):
-        if not self.buffered:
+        for idx in range(1, self.frame_count):
             ret, frame_bgr = self.video.read()
-            if ret:
-                frame_grey = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2GRAY)
-                return frame_bgr, frame_grey
-            else:
-                return None, None
-        else:
-            frames = (self.frames_bgr[self.buff_idx], self.frames_grey[self.buff_idx])
-            self.buff_idx += 1
-            return frames
+            frame_grey = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2GRAY)
+            self.frames_bgr[idx] = frame_bgr
+            self.frames_grey[idx] = frame_grey
+
+    def current_frame(self) -> (np.ndarray, np.ndarray):
+        return self.frames_bgr[self._frame_idx], self.frames_grey[self._frame_idx]
+
+    def next_frame(self) -> (np.ndarray, np.ndarray):
+        self._frame_idx = (self._frame_idx + 1) % self.frame_count
+        return self.current_frame()
 
     def reset_frames(self):
-        self.buff_idx = 0
+        self._frame_idx = 0
+
+
--- src/gui/kivy_gui/gui_app.py
+++ src/gui/kivy_gui/gui_app.py
@@ -1,48 +1,27 @@
-from abc import ABC, abstractmethod
-
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.video import Image
 
-from src.data.video import AbstractVideo, OpenCvVideo
+from src.data.video import OpenCvVideo
 
 
 class GuiLayout(GridLayout):
     pass
 
 
-class VideoPlayer(ABC):
-    def __init__(self):
-        super().__init__()
-
-    @abstractmethod
-    def load_video(self, video: AbstractVideo):
-        pass
-
-    @abstractmethod
-    def play_video(self):
-        pass
-
-    @abstractmethod
-    def stop_video(self):
-        pass
-
-    @abstractmethod
-    def trigger_video(self):
-        pass
-
-
-class OpenCvVideoPlayer(VideoPlayer, Image):
+class OpenCvVideoPlayer(Image):
     """OpenCV video player.
 
     Designed to interface with OpenCvVideo objects.
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.playing = False
+        self.video: OpenCvVideo = None
 
     def load_video(self, video: OpenCvVideo):
-        pass
+        self.video = video
 
     def play_video(self):
         pass
@@ -51,6 +30,12 @@ class OpenCvVideoPlayer(VideoPlayer, Image):
         pass
 
     def trigger_video(self):
+        if self.playing:
+            self.stop_video()
+        else:
+            self.play_video()
+
+    def reset_video(self):
         pass
 
 
@@ -64,5 +49,5 @@ class GuiApp(App):
         self.gui_layout.add_widget(self.video_player)
         return self.gui_layout
 
-    def load_video(self, video: AbstractVideo):
+    def load_video(self, video: OpenCvVideo):
         self.video_player.load_video(video)
